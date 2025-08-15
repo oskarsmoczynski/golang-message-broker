a943aaea4922abb23612ef400a51340b795054b8
oskarsmoczynski <smoczynskioskar@gmail.com>
2025-08-15 12:49:33 +0200

    Added custom error to topics

--- internal/broker/core/topic.go
+++ internal/broker/core/topic.go
@@ -2,40 +2,42 @@ package core
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/oskarsmoczynski/golang-message-broker/internal/broker/queue"
 	"github.com/oskarsmoczynski/golang-message-broker/internal/broker/utils"
 )
 
+var ErrAlreadyExists = errors.New("already exists")
+
 type Topic struct {
-    Queue *queue.Queue
+	Queue *queue.Queue
 }
 
 type TopicManager struct {
-    topics map[string]*Topic
+	topics map[string]*Topic
 }
 
 func NewTopicManager() *TopicManager {
-    return &TopicManager{
-        topics: make(map[string]*Topic, 0),
-    }
+	return &TopicManager{
+		topics: make(map[string]*Topic, 0),
+	}
 }
 
 func (tm *TopicManager) GetTopicByName(name string) (*Topic, bool) {
-    if !utils.IsValidTopicName(name) {
-        return nil, false
-    }
-    topic, ok := tm.topics[name]
-    return topic, ok
+	if !utils.IsValidTopicName(name) {
+		return nil, false
+	}
+	topic, ok := tm.topics[name]
+	return topic, ok
 }
 
-
 func (tm *TopicManager) CreateTopic(name string) error {
-    if _, exists := tm.GetTopicByName(name); exists {
-        return errors.New("Topic already exists")
-    }
-    tm.topics[name] = &Topic{
-        Queue: queue.NewQueue(),
-    } 
-    return nil
+	if _, exists := tm.GetTopicByName(name); exists {
+		return fmt.Errorf("failed to create topic %s: %w", name, ErrAlreadyExists)
+	}
+	tm.topics[name] = &Topic{
+		Queue: queue.NewQueue(),
+	}
+	return nil
 }
